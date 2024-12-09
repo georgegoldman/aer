5b69093d753e3b1d2740214de592ccdeb06c51cb
georgegoldman <georgegoldmanjohn.o@gmail.com>
2024-12-09 14:43:42 +0100

    fixed

--- app/services.py
+++ app/services.py
@@ -1,27 +1,27 @@
-from openai import OpenAI
+
 from typing import Optional
 from dotenv import load_dotenv
 import os
+import google.generativeai as genai #type: ignore
 
 load_dotenv()
 
-
-
-client = OpenAI(api_key=os.getenv("VARSCON_Test"))
-
+# print(os.getenv("VARSCON_Test"))
 
 
 def get_chatgpt_response(context: Optional[str], message: str) -> str:
+    api_key = os.getenv("AI_API_KEY")
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
     messages = []
     if context:
         messages.append({"role": "system", "content": context})
     messages.append({"role": "user", "content": message})
 
     try:
-        response = client.chat.completions.create(
-            model=os.getenv("MODEL"), #type: ignore
-            messages=messages #type: ignore
-        )
-        return response.choices[0].message.content #type: ignore
+        completion = model.generate_content(message)
+        response = completion.text
+        return response #type: ignore
     except Exception as e:
         return f"Error: {str(e)}" #type: ignore
